ef90d25a3a23971e48ef9f32b668403fc3b9c059
Robert Tyssen <rob.tyssen@gmail.com>
2024-01-28 22:28:14 -0500

    Added better placeholder for project section

--- src/components/sections/Projects.tsx
+++ src/components/sections/Projects.tsx
@@ -1,10 +1,15 @@
-import { Box, Typography } from '@mui/material'
+import { Box, Link, Typography } from '@mui/material'
 
 const Projects = () => {
   return (
     <Box sx={{ py: 4 }} id='projects'>
-      <Typography variant='h2'>My Projects</Typography>
-      <Typography>TO BE COMPLETED</Typography>
+      <Typography variant='h2' gutterBottom>My Projects</Typography>
+      <Typography>
+        This section is currently under construction - check back later to see more of
+        what I'm working on. In the meantime, feel free to browse my 
+        public repos on <Link href='https://github.com/Robert-Tyssen'>Github</Link>!
+        I also have some private repos that I'd be happy to discuss.
+      </Typography>
     </Box>
   )
 }
